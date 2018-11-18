68d47dc15d947ef684167428d6b59541cbf6a0df
Ollipekka Väänänen <Ollipekka.vaananen@helsinki.fi>
2018-11-18 15:45:23 +0200

    projekti herokuun

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 # coding=utf-8
 from app import app
+#import logging
+#import sys
+
+#app.logger.addHandler(logging.StreamHandler(sys.stdout))
+#app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run(debug=True)
